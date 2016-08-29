3fad10b62b1174e16be478eb4f237a8f0d562c16
anirori <hosodasyun789@gmail.com>
2016-08-29 09:44:54 +0900

    second commit

---
+++ src/resource.js
@@ -0,0 +1,25 @@
+var res = {
+  chara_princessselect_10: "res/char/chara_princessselect_10.png",
+  chara_princessselect_11: "res/char/chara_princessselect_11.png",
+  chara_princessselect_12: "res/char/chara_princessselect_12.png",
+  ss_BattleScene_bg1: "res/field/ss_BattleScene_bg1.jpg",
+  ss_BattleScene_bg2: "res/field/ss_BattleScene_bg2.jpg",
+  ss_BattleScene_bg3: "res/field/ss_BattleScene_bg3.jpg",
+  chara_enemy_1: "res/char/chara_enemy_1.png",
+  chara_enemy_2: "res/char/chara_enemy_2.png",
+  chara_enemy_3: "res/char/chara_enemy_3.png",
+  chara_enemy_4: "res/char/chara_enemy_4.png",
+  chara_enemy_5: "res/char/chara_enemy_5.png",
+  chara_enemy_6: "res/char/chara_enemy_6.png",
+  fire_particl1: "res/effect/Fireparticle1_texture.plist",
+  fire_particl2: "res/effect/Fireparticle2_texture.plist",
+  laser_particl1: "res/effect/particle_texture.plist",
+  laser_particl2: "res/effect/particle2_texture.plist",
+  fire2_particl1: "res/effect/Fireparticle4_texture.plist",
+  fire2_particl2: "res/effect/Fireparticle5_texture.plist",
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
